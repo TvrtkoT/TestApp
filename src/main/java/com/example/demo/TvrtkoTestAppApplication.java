410471d03cae34069ae7d447e4d0818a2a9ccb86
ticinovic.tvrtko <ticinovic.tvrtko@gmail.com>
2022-01-31 09:28:28 +0100

    Pushing initial commit

---
+++ src/main/java/com/example/demo/TvrtkoTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvrtkoTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvrtkoTestAppApplication.class, args);
+	}
+
+}
